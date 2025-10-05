a09dd250b1ef3c280be9a4bef4f5ab2e729eb8c9
jannes <jannesdrijkoningen@gmail.com>
2025-10-05 11:56:05 +0200

    fix js entrypoints
    Our controllers were being registered before live components, which
    caused issues in controllers in live components that modified the DOM
    not being re-executed.

--- forumify/forumify-platform/1.0/assets/admin.js
+++ forumify/forumify-platform/1.0/assets/admin.js
@@ -1,5 +1,5 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
-import '@forumify/forumify-platform/admin.js';
+import admin from '@forumify/forumify-platform/admin.js';
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
@@ -8,5 +8,6 @@ export const app = startStimulusApp(require.context(
   /\.[jt]sx?$/
 ));
 
+admin(app);
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
--- forumify/forumify-platform/1.0/assets/frontend.js
+++ forumify/forumify-platform/1.0/assets/frontend.js
@@ -1,5 +1,5 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
-import '@forumify/forumify-platform/frontend.js';
+import frontend from '@forumify/forumify-platform/frontend.js';
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
@@ -8,5 +8,6 @@ export const app = startStimulusApp(require.context(
   /\.[jt]sx?$/
 ));
 
+frontend(app);
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
